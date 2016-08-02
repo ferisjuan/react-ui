5bf53cf321f9c62e33301ecca199e834b54cd07d
Juan Feris <feris.juan@gmail.com>
2016-08-01 21:24:39 -0500

    added typography.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import 'babel-polyfill';
-// import $ from 'jquery';
 import React from 'react'
 import {render} from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import Cont from './components/Cont.jsx'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Cont from './components/Cont.jsx';
+import Typography from 'typography';
+import lincolnTheme from 'typography-theme-lincoln';
+
+const typography = new Typography(lincolnTheme);
+
+// Output CSS as string.
+typography.toString()
+
+// Or insert styles directly into the <head> (works well for client-only
+// JS web apps.
+typography.injectStyles()
 
 const App = () => (
   <MuiThemeProvider>
